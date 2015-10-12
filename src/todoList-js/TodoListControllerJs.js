c357ca7056b55d494d1e9e5fb7f64f21a13fc0d3
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2015-10-12 14:06:51 +0200

    fixed visual studio solution and added some comments

--- src/todoList-js/TodoListControllerJs.js
+++ src/todoList-js/TodoListControllerJs.js
@@ -1,20 +1,23 @@
+/**
+ * a controller for the todo list: we're going to use controllerAs syntax
+ */
 TodoListJsController.$inject = ["TodoListJsService"];
 function TodoListJsController(todoListService) {
 
-	var _todoListService = todoListService;
+    var _todoListService = todoListService;
 
-	this.todos = _todoListService.todos;
+    this.todos = _todoListService.todos;
 
-	this.addTodo = function (task) {
-		_todoListService.addTodo(task);
-	}
+    this.addTodo = function (task) {
+        _todoListService.addTodo(task);
+    }
 
-	this.removeTodo = function (id) {
-		// ask for confirmation
-		if (confirm("Do you really want to delete task: " + id + "?")) {
-			_todoListService.removeTodo(id);
-		}
-	}
+    this.removeTodo = function (id) {
+        // ask for confirmation
+        if (confirm("Do you really want to delete task: " + id + "?")) {
+            _todoListService.removeTodo(id);
+        }
+    }
 }
 
 angular.module("app")
